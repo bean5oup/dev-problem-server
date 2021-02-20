ced66930762c0b74b757e3c05400035b3e86c644
bean3oup <a01022757528@gmail.com>
2021-02-19 22:51:41 -0800

    docker_manage.py

--- docker_manage.py
+++ docker_manage.py
@@ -3,37 +3,41 @@ import os
 
 client = docker.from_env()
 
-#client.images.build(paht='./', tag='test')
-#os.chdir('/Users/kangwoosun/Desktop/Woosun/github/dev-problem-server')
-#print(client.images.get('test'))
+def build_images():	
 
-def build_images():
-	for i in os.listdir('./problems'):
-		client.images.build(path=f'./problems/{i}', tag=f'{i}', rm=True)	
+	for problem in os.listdir('./problems'):
+		client.images.build(path=f'./problems/{problem}', tag=f'{problem}', rm=True)	
+
+	for image in client.images.list():
+		if image.tags == []:
+			client.images.prune()
+
 
 def run_container(user_token, problem, port):
-	client.containers.run(problem, detach=True, name=user_token, ports={'12370/tcp':port})
+	
+	client.containers.run(problem, detach=True, name=user_token, ports={'12370/tcp':port}, remove=True)
+
 
 def stop_container(user_token):
+	
 	for container in client.containers.list():
 		if container.name == user_token:
-			container.stop()
+			container.stop(timeout=100)
+
 
 def remove_container(user_token):
+	
 	for container in client.containers.list(all):
 		if container.name == user_token:
 			container.remove()
 
+
 #======test=======
 #build_images()
 #run_container(111, "pwn1", 12370)
-#run_container(222, "pwn1", 12371)
+#run_container(222, "web1", 12371)
 
 #stop_container('111')
 #stop_container('222')
-
-#remove_container('111')
-#remove_container('222')
-
 
 
---
+++ problems/web1/app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '''
+    <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <title>flask debugger</title>
+    </head>
+    <body>
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="/">flask debugger</a>
+        </div>
+        <div id="navbar">
+          <ul class="nav navbar-nav">
+            <li><a href="/upload">upload</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav><br/><br/> 
+    </body>
+    '''
+@app.route("/upload")
+def upload():
+    return '''
+    <html>
+    <body>
+    <form action = "/fileupload" method = "POST"
+        enctype = "multipart/form-data">
+        <input type = "file" name = "file"/>
+        <input type = "submit"/>
+    </form>
+    </body>    
+    </html>
+    '''
+
+@app.route('/fileupload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+      f = request.files['file']
+      ext = f.filename[-3:]
+      if ext == 'txt':
+        f.save(f.filename)
+        return f'''<form action = "/{f.filename}" method = "POST"
+        enctype = "multipart/form-data">
+        <input type = "submit"/>
+        </form>'''
+    return 'only .txt'
+
+@app.route('/<path:file>', methods=['POST'])
+def file(file):
+	return open(file).read()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=12370, debug=True)
