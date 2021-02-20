183eac57efda76263182cd476eb800f144556199
Kangwoosun <kws981924@gmail.com>
2021-02-20 21:39:02 +0900

    Add multThreading in docker_manage.py & edit option in serve.py

--- problem_server.py
+++ problem_server.py
@@ -3,12 +3,70 @@ import portfinder
 import threading
 
 
+class server:
 
-class server
+    def __init__(self, sock=None, host="127.0.0.1", port=None, max, file):
+        if sock is None:
+            self.sock = socket.socket(
+                            socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
 
-	def start():
+        self.sock.stsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+
+        if port is None:
+        	print("[-] None port error : (server, __init__)")
+        	return -1
+
+        self.sock.bind((host, port))
+
+    def waiting(self):
+
+    	while True:
+    		try:
+
+    			client_socket, addr = self.sock.accept()
+
+    		except KeyboardInterrupt:
+    			self.sock.close()
+    			print("[*] KeyboardInterrupt")
+    			break
+
+    		print("[+] Receive request")
+
+    		t = threading.Thread(target=receiveRequest, args=(client_socket,addr))
+    		t.daemon = True
+    		t.start()
+
+    	return 0
+
+
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+
+    def mysend(self, msg):
+        totalsent = 0
+        while totalsent < MSGLEN:
+            sent = self.sock.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+
+    def myreceive(self):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < MSGLEN:
+            chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return b''.join(chunks)
+
+
+    def receiveRequest(client_socket, addr):
 
 
 
-
-	return
+    	return
--- serve.py
+++ serve.py
@@ -1,20 +1,28 @@
 import argparse
-
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--port", help="Port for problem_server to listen on", default=8080)
 parser.add_argument("--max", help="Set the maximum number of dockers to run", default=20)
-parser.add_argument("--file", help="Name of problem's docker file", action="store_true")
+parser.add_argument("--file", help="location of problem's docker file", default="setting")
 
-args = parser.prase_args()
+args = parser.parse_args()
 
 
-if args.file:
+if os.path.relpath(args.file):
 
+
+	print("[+] port : {}".format(args.port))
+	print("[+] max : {}".format(args.max))
+	print("[+] file : {}".format(args.file))
 	
 
+	from problem_server import *
 
-else:
+	problem_server = server()
 	
-	print("[-] please setting location of docker file")
+
+else:	
+	print("[-] Not exist docker file : {}".format(args.file))
 	exit(-1)
+
